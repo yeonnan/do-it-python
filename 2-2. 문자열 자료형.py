d57dc46d75cd63ac035ea03b39e3c1e786927333
yeonnan <145152442+yeonnan@users.noreply.github.com>
2024-10-06 00:46:38 +0900

    2-2. 문자열 자료형

--- 2-2. 문자열 자료형.py
+++ 2-2. 문자열 자료형.py
@@ -119,3 +119,77 @@ print('{0:<10}'.format('hi'))
 
 '''가운데 정렬'''
 print('{0:^10}'.format('hi'))   #     hi    
+
+'''공백 채우기'''
+print('{0:=^10}'.format('hi'))      # ====hi====
+print('{0:!<10}'.format('hi'))      # hi!!!!!!!!
+
+'''{또는} 문자 표현하기'''
+print('{{and}}'.format())   # {and}
+
+'''f 문자열 포매팅'''
+name = '파이썬'
+age = 25
+print(f'나의 이름은 {name}이고, 나이는 {age}')  # 나의 이름은 파이썬이고, 나이는 25
+print(f'나는 내년이면 {age+1} 살이 된다.')      # 나는 내년이면 26 살이 된다.
+
+# 딕셔너리
+d = {'name':'안녕', 'age':25}
+print(f"나의 이름은 {d['name']}이고, 나이는 {d['age']}입니다.")     # 나의 이름은 안녕이고, 나이는 25입니다.
+
+print(f'{{and}}')   # {and}
+
+'''문자열 관련 함수'''
+'''문자 개수 세기 - count'''
+a = 'hobby'
+print(a.count('b'))     # 2
+
+'''위치 알려주기 - find'''
+a = 'python is the best choice'
+print(a.find('b'))      # 14 : 문자열에서 b거 처음으로 나온 위치 반환
+print(a.find('k'))      # -1 : 찾는 문자나 문자열이 존재하지 않으면 -1 반환
+
+'''위치 알려주기 - index'''
+a = 'Life is too short'
+print(a.index('t'))     # 8
+# print(a.index('k'))     # ValueError: substring not found : k가 없으므로 오류 발생
+
+'''문자열 삽입 - join'''
+print(','.join('abcd'))     # a,b,c,d
+
+# 리스트나 튜플도 입력으로 사용할 수 있다.
+print(','.join(['a','b','c','d']))      # # a,b,c,d
+
+'''소문자를 대문자로 바꾸기 - upper'''
+a = 'hi'
+print(a.upper())    # HI
+
+'''대문자를 소문자로 바꾸기 - lower'''
+a = 'HI'
+print(a.lower())    # hi
+
+'''왼쪽 공백 지우기 - lstrip'''
+a = ' hi '
+print(a.lstrip())
+
+'''오른쪽 공백 지우기 - rstrip'''
+a = ' hi '
+print(a.rstrip())    #  hi
+
+'''양쪽 공백 지우기 - strip'''
+a = ' hi '
+print(a.strip())
+
+'''문자열 바꾸기 - replace'''
+a = 'life if too short'
+print(a.replace('life', 'your leg'))    # your leg if too short
+
+'''문자열 나누기 - split'''
+a = 'life if too short'
+print(a.split())    # ['life', 'if', 'too', 'short'] : 공백을 기준으로 문자열 나눔
+b = 'a:b:c:d'
+print(b.split(':'))     # ['a', 'b', 'c', 'd'] : :를 기준으로 문자열 나눔
+
+# 문자열은 값을 변경할 수 없는 immutable (불변성) 자료형이다.
+# upper, lower, join, lstrip, rstrip, strip, replace, split 함수는 immutable 규칙이 적용되어
+# 문자열 자체의 값이 변경되는게 아니라 변경된 값을 리턴한다.
