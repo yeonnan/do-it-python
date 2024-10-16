acbd04710b8d889c0601e1267bf58d5acbafae13
yeonnan <145152442+yeonnan@users.noreply.github.com>
2024-10-16 21:37:01 +0900

    6. 파이썬 실행

---
+++ 6-1. 구구단.py
@@ -0,0 +1,9 @@
+def multiplication(n):
+    result = []
+    i = 1
+    while i < 10:
+        result.append(n*i)
+        i = i+1
+    print(result)
+
+multiplication(2)
---
+++ 6-3. 게시판 페이징.py
@@ -0,0 +1,10 @@
+def get_total_page(m,n):
+    if m%n == 0:
+        return m // n
+    else:
+        return m // n + 1
+
+print(get_total_page(5, 10))
+print(get_total_page(15, 10))
+print(get_total_page(25, 10))
+print(get_total_page(30, 10))
